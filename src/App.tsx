0c56ea881196849c6245912b3a5ba1946b9fd70a
k4l4p <kalapwong@gmail.com>
2023-03-04 11:29:25 +0800

    impl: page stage state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import React, { useState } from "react"
+import React, { useContext, useEffect, useState } from "react";
 import CreateNFT from "./components/CreateNFT";
-import Header from "./components/Header"
-import MintSuccess from "./components/MintSuccess"
-import { ConnectPage } from "./components/WalletConnection";
+import Header from "./components/Header";
+import MintSuccess from "./components/MintSuccess";
+import { ConnectCtx, ConnectModal, ConnectPage, ConnectRoot } from "./components/WalletConnection";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const connectCtx = useContext(ConnectCtx);
+  const [stage, setStage] = useState<'connect' | 'create' | 'success'>('connect')
+
+  const setMintSuccess = () => {
+    setStage('success')
+  }
+  useEffect(() => {
+    if (connectCtx.isConnected) {
+      setStage('create')
+    }
+    console.log(connectCtx.isConnected)
+  }, [connectCtx.isConnected])
+  
   return (
-    <main className="font-dm-sans">
-      <Header />
-      {/* <ConnectPage /> */}
-      {/* <CreateNFT /> */}
-      <MintSuccess />
-    </main>
+    <>
+      <main className="font-dm-sans">
+        <Header />
+        {
+        stage === 'connect' && <ConnectPage />
+        }
+        {
+        stage === 'create' &&  <CreateNFT proceed={setMintSuccess} />
+        }
+        {
+        stage === 'success' &&  <MintSuccess />
+        }
+        
+      </main>
+      <ConnectModal />
+    </>
   );
 }
 
