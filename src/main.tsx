5c6e0892671290b508690bd647610314d5b427cc
k4l4p <kalapwong@gmail.com>
2023-03-20 23:16:00 +0800

    impl: tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ConnectRoot } from "./components/WalletConnection";
+import { SettingsProvider } from "./contexts/Settings"
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root")!)
+
+root.render(
   <React.StrictMode>
+    <SettingsProvider>
     <ConnectRoot>
       <App />
     </ConnectRoot>
+    </SettingsProvider>
   </React.StrictMode>
 );
