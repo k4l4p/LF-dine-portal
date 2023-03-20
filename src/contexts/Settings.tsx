dd9737a3b2e5636611dc2414cc286efcfe7e785b
k4l4p <kalapwong@gmail.com>
2023-03-20 23:16:13 +0800

    chores: add taquito

---
+++ src/contexts/Settings.tsx
@@ -0,0 +1,25 @@
+import { NetworkType } from '@airgap/beacon-sdk'
+import constate from 'constate'
+import { useState } from 'react'
+
+export const [
+  SettingsProvider,
+  useAppName,
+  useEndpoint,
+  useNetwork,
+  useContractAddress,
+] = constate(
+  () => {
+    const [settings] = useState({
+      app_name: 'My DApp',
+      endpoint: 'https://ghostnet.ecadinfra.com',
+      network: NetworkType.GHOSTNET,
+      contract: 'KT1...',
+    })
+    return settings
+  },
+  (v) => v.app_name,
+  (v) => v.endpoint,
+  (v) => v.network,
+  (v) => v.contract
+)
