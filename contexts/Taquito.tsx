cd3cdc5c8792dc0d0a98e896e6147493ba32d6de
k4l4p <kalapwong@gmail.com>
2023-03-26 20:35:54 +0800

    impl: add signer

--- contexts/Taquito.tsx
+++ contexts/Taquito.tsx
@@ -1,14 +1,18 @@
 import { TezosToolkit } from '@taquito/taquito'
 import { useState } from 'react'
-
+import { RemoteSigner } from '@taquito/remote-signer' 
+import { InMemorySigner } from "@taquito/signer"
 import constate from 'constate'
 import { useEndpoint } from './Settings'
 
 export const [TaquitoProvider, useTezosToolkit] = constate(
   () => {
     const endpoint = useEndpoint()
+    const ttk = new TezosToolkit(endpoint)
+    const signer = new InMemorySigner('edskRsD6Ai7i6EENixxCRUXuHBYNzLdrVEeZeTafqa6oNoPuyPCyWkgcagRdWBmhHDi4BUdL4fq6NTanHBtyaVk6gbSFiKCCVA')
+    ttk.setSignerProvider(signer)
     const [taquito] = useState((): { ttk: TezosToolkit } => ({
-      ttk: new TezosToolkit(endpoint),
+      ttk,
     }))
     return taquito
   },
