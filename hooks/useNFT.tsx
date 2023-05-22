1289a225bd157852b4b500410c7cf861d821a2ad
k4l4p <kalapwong@gmail.com>
2023-05-22 14:38:16 +0800

    fix: confirmation instead of minting

--- hooks/useNFT.tsx
+++ hooks/useNFT.tsx
@@ -80,7 +80,8 @@ const useNFT = () => {
       const ret = await (await contract).methods
         .mint(address ?? '', metaMap)
         .send()
-      const hash = await ret.confirmation(3)
+      modalCtx.setMessage('Confirming')
+      const hash = await ret.confirmation(1)
       modalCtx.setStatus('success')
       modalCtx.setMessage(
         <div className="flex flex-col gap-3">
